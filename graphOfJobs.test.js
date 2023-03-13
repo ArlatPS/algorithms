827f47864388d1e175c5593f8fc1295f1b16f8f9
ArlatPS <przemekx10@gmail.com>
2023-03-13 21:45:59 +0100

    graph tested

--- graphOfJobs.test.js
+++ graphOfJobs.test.js
@@ -43,4 +43,19 @@ const findMostCommonTitle = (myId, degreesOfSeparation) => {
   return winner.job;
 };
 
-console.log(findMostCommonTitle(1, 7));
+describe("graph", function () {
+  test("user 30 with 2 degrees of separation", () => {
+    expect(findMostCommonTitle(30, 2)).toBe("Librarian");
+  });
+
+  test("user 11 with 3 degrees of separation", () => {
+    expect(findMostCommonTitle(11, 3)).toBe("Graphic Designer");
+  });
+
+  test("user 307 with 4 degrees of separation", () => {
+    expect(findMostCommonTitle(306, 4)).toBe("Pharmacist");
+  });
+  test("user 1 with 7 degrees of separation", () => {
+    expect(findMostCommonTitle(1, 7)).toBe("Geological Engineer");
+  });
+});
